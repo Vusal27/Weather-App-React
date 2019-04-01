2e5e8983f0481e4ef76bc323a31ce9e81e4e8820
Vusal27 <nuruva02@gmail.com>
2019-04-01 13:21:52 +0300

    Updates

---
+++ precache-manifest.74c523b0919956ef5f3fdd4dd26af8c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8daeab6e96b2bed2beb4",
+    "url": "/Vusal27/Weather-App-React/static/js/runtime~main.27798215.js"
+  },
+  {
+    "revision": "afd17ebfef094dc3fd33",
+    "url": "/Vusal27/Weather-App-React/static/js/main.c3d314d9.chunk.js"
+  },
+  {
+    "revision": "a65f59a93b26c2de44ac",
+    "url": "/Vusal27/Weather-App-React/static/js/2.bbac7bca.chunk.js"
+  },
+  {
+    "revision": "afd17ebfef094dc3fd33",
+    "url": "/Vusal27/Weather-App-React/static/css/main.c91f85fa.chunk.css"
+  },
+  {
+    "revision": "d43b27e1b958163f7facfdf933f124d5",
+    "url": "/Vusal27/Weather-App-React/index.html"
+  }
+];
